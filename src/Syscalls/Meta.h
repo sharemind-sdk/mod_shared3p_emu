b19792d5e9ba6b73899ed03c304e49da1bfbdd44
Reimo Rebane <reimo.rebane@cyber.ee>
2014-12-01 18:11:15 +0200

    Syscalls/Meta: Added a binary arithmetic function for syscalls where the right-hand side is a public vector.

--- src/Syscalls/Meta.h
+++ src/Syscalls/Meta.h
@@ -14,6 +14,7 @@
 
 #include "Common.h"
 #include "../Shared3pPDPI.h"
+#include "../VMReferences.h"
 
 /**
  * Meta-syscalls for many common cases.
@@ -69,10 +70,62 @@ SHAREMIND_MODULE_API_0x1_SYSCALL(binary_vec,
             return SHAREMIND_MODULE_API_0x1_GENERAL_ERROR;
 
         return SHAREMIND_MODULE_API_0x1_OK;
-    } catch (const std::bad_alloc &) {
-        return SHAREMIND_MODULE_API_0x1_OUT_OF_MEMORY;
     } catch (...) {
-        return SHAREMIND_MODULE_API_0x1_GENERAL_ERROR;
+        return catchModuleApiErrors();
+    }
+}
+
+/**
+ * SysCall: binary_public_vec<T1, T2, T3, Protocol>
+ * Args:
+ *      0) uint64[0]     pd index
+ *      1) p[0]          LHS handle
+ *      2) p[0]          RHS handle
+ *      2) p[0]          output handle
+ * Precondition:
+ *      LHS handle is a vector of type T1.
+ *      RHS handle is a vector of type T2.
+ *      Output handle is a vector of type T3.
+ */
+template <typename T1, typename T2, typename T3, typename Protocol>
+SHAREMIND_MODULE_API_0x1_SYSCALL(binary_public_vec,
+                                 args, num_args, refs, crefs,
+                                 returnValue, c)
+{
+    VMHandles handles;
+    if (!SyscallArgs<3, false, 0u, 1u>::check(num_args, refs, crefs, returnValue) ||
+            !handles.get(c, args))
+    {
+        return SHAREMIND_MODULE_API_0x1_INVALID_CALL;
+    }
+
+    try {
+        Shared3pPDPI * const pdpi = static_cast<Shared3pPDPI*>(handles.pdpiHandle);
+
+        void * const lhsHandle = args[1u].p[0u];
+        void * const resultHandle = args[2u].p[0u];
+
+        if (!pdpi->isValidHandle<T1>(lhsHandle) ||
+                !pdpi->isValidHandle<T3>(resultHandle))
+        {
+            return SHAREMIND_MODULE_API_0x1_GENERAL_ERROR;
+        }
+
+        const s3p_vec<T1> & param1 = *static_cast<s3p_vec<T1>*>(lhsHandle);
+        const immutable_vm_vec<T2> param2(crefs[0u]);
+        s3p_vec<T3> & result = *static_cast<s3p_vec<T3>*>(resultHandle);
+
+        if (param1.size() <= crefs[0u].size / sizeof(T2) ||
+                param1.size() != result.size())
+            return SHAREMIND_MODULE_API_0x1_INVALID_CALL;
+
+        Protocol protocol(*pdpi);
+        if (!protocol.invoke(param1, param2, result, typename value_traits<T1>::value_category()))
+            return SHAREMIND_MODULE_API_0x1_GENERAL_ERROR;
+
+        return SHAREMIND_MODULE_API_0x1_OK;
+    } catch (...) {
+        return catchModuleApiErrors();
     }
 }
 
@@ -92,6 +145,24 @@ SHAREMIND_MODULE_API_0x1_SYSCALL(binary_arith_vec,
                                  returnValue, c)
 {
     return binary_vec<T, T, T, Protocol>(args, num_args, refs, crefs, returnValue, c);
+}
+
+/**
+ * SysCall: binary_arith_public_vec<T, Protocol>
+ * Args:
+ *      0) uint64[0]     pd index
+ *      1) p[0]          vector handle (lhs)
+ *      2) p[0]          vector handle (rhs)
+ *      3) p[0]          output handle
+ * Precondition:
+ *      All handles are valid vectors of type T.
+ */
+template <typename T, typename Protocol>
+SHAREMIND_MODULE_API_0x1_SYSCALL(binary_arith_public_vec,
+                                 args, num_args, refs, crefs,
+                                 returnValue, c)
+{
+    return binary_public_vec<T, T, T, Protocol>(args, num_args, refs, crefs, returnValue, c);
 }
 
 /**
@@ -136,10 +207,8 @@ SHAREMIND_MODULE_API_0x1_SYSCALL(unary_vec,
             return SHAREMIND_MODULE_API_0x1_GENERAL_ERROR;
 
         return SHAREMIND_MODULE_API_0x1_OK;
-    } catch (const std::bad_alloc &) {
-        return SHAREMIND_MODULE_API_0x1_OUT_OF_MEMORY;
     } catch (...) {
-        return SHAREMIND_MODULE_API_0x1_GENERAL_ERROR;
+        return catchModuleApiErrors();
     }
 }
 
@@ -193,10 +262,8 @@ SHAREMIND_MODULE_API_0x1_SYSCALL(nullary_vec,
             return SHAREMIND_MODULE_API_0x1_GENERAL_ERROR;
 
         return SHAREMIND_MODULE_API_0x1_OK;
-    } catch (const std::bad_alloc &) {
-        return SHAREMIND_MODULE_API_0x1_OUT_OF_MEMORY;
     } catch (...) {
-        return SHAREMIND_MODULE_API_0x1_GENERAL_ERROR;
+        return catchModuleApiErrors();
     }
 }
 
